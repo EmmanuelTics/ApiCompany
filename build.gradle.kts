930c172cdc1f9fdfbd96e3f817b5b46eaf584171
Emmanuel <emacal200901@gmail.com>
2025-03-04 00:03:33 -0600

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    kotlin("jvm") version "1.9.10"
+    application
+
+     kotlin("plugin.serialization") version "1.9.0"  // Esto habilita la serialización de Kotlin
+
+
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+      maven { url = uri("https://repo.maven.apache.org/maven2") }
+
+}
+
+application {
+    mainClass.set("ApplicationKt")
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core:2.3.4")
+    implementation("io.ktor:ktor-server-netty:2.3.4")
+    implementation("io.ktor:ktor-server-content-negotiation:2.3.4")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.4")
+    implementation("org.litote.kmongo:kmongo:4.10.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    testImplementation("io.ktor:ktor-server-tests:2.3.4")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.8.20")
+          implementation("io.ktor:ktor-server-status-pages:2.3.0") 
+             implementation ("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+                 implementation("io.ktor:ktor-server-cors:2.3.0")
+                 
+   
+}
+
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+tasks.withType<JavaExec> {
+    systemProperty("file.encoding", "UTF-8")
+}
---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,60 @@
+// Application.kt
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.statuspages.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.routing.*
+import routes.employeeRoutes // Importa las rutas de employees
+import routes.userRoutes // Importa las rutas de employees
+import io.ktor.server.response.*
+import io.ktor.server.request.*
+import io.ktor.http.*
+import io.ktor.server.plugins.cors.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, module = Application::module).start(wait = true)
+}
+
+fun Application.module() {
+    // Configuración de la negociación de contenido para JSON
+    install(ContentNegotiation) {
+        json() // Serialización/Deserialización de JSON
+    }
+  install(CORS) {
+
+    anyHost() // Permite solicitudes desde cualquier host (origen)
+
+    // Configura los métodos permitidos
+    allowMethod(HttpMethod.Get)
+    allowMethod(HttpMethod.Post)
+    allowMethod(HttpMethod.Put)
+    allowMethod(HttpMethod.Delete)
+
+    // Configura los encabezados permitidos
+    allowHeader(HttpHeaders.ContentType)
+    allowHeader(HttpHeaders.Authorization)
+    allowHeader(HttpHeaders.Accept)
+
+    // Habilita el envío de credenciales si es necesario
+    allowCredentials = true
+}
+ 
+   
+    
+
+
+    // Manejo global de excepciones
+    install(StatusPages) {
+        exception<Throwable> { call, cause -> 
+            call.respond(HttpStatusCode.InternalServerError, cause.message ?: "Unknown error")
+        }
+    }
+
+    // Llamada a las rutas de employees
+    routing {
+        employeeRoutes()
+         userRoutes()  // Aquí se llaman las rutas definidas en RoutesEmployees.kt
+    }
+}
